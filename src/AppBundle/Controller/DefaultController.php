548048b17c6f00711b20503bee49fb3f0c2c45a9
Thomas Hubert <mr.thomas.hubert@gmail.com>
2018-06-14 21:03:26 +0200

    pure code

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,15 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/pokedex", name="pokedex")
+     */
+    public function pokedexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/pokedex.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
 }
